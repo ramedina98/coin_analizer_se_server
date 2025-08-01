8dfa1e4d1777d521d32e050f45230652ef42a421
ramedina98 <rmedinamartindelcampo@gmail.com>
2025-08-01 11:45:29 -0600

    Create CFDIs API getCFDIs

--- app/core/database/schemas/PricesSchemas.py
+++ app/core/database/schemas/PricesSchemas.py
@@ -1,10 +1,8 @@
-from typing import Optional
 from pydantic import BaseModel
-from datetime import datetime
 
 class HistoricalPriceBase(BaseModel):
   coinName: str
-  date: datetime
+  date: str
   price: float
   volume: float
 
